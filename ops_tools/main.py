f1b63d3e0370b039f6b33665c8cf5553062bc940
Antony Sequeira <asequeira.open@gmail.com>
2025-04-17 13:45:56 -0700

    uv start

---
+++ ops_tools/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from ops-tools!")
+
+
+if __name__ == "__main__":
+    main()
--- scripts/install-all.sh
+++ scripts/install-all.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -e
 # TODO 0 AAS
+curl -LsSf https://astral.sh/uv/install.sh | sh
 ./aliases
 ./verify_all
